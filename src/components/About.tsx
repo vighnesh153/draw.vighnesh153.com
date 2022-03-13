1d64ba82afbb2a68c4630a7e945803131adc41f2
Vighnesh Raut <vighnesh.raut13@gmail.com>
2022-03-13 20:15:59 +0530

    refactor: links in About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,15 +5,19 @@
 import React from 'react';
 
 export function About(): JSX.Element {
+  const getLink = (text: string, link: string) => {
+    return (
+      <a href={link} target="_blank" rel="noreferrer" style={{ fontWeight: 'bold' }}>
+        {text}
+      </a>
+    );
+  };
+
   return (
-    <div style={{ padding: '1rem 3rem', textAlign: 'center' }}>
+    <div style={{ padding: '1rem 3rem', textAlign: 'center', lineHeight: 1.5 }}>
       <h1>A Drawing App</h1>
-      <p>
-        Made with ❤️ by{' '}
-        <a href="https://vighnesh153.com" target="_blank" rel="noreferrer">
-          Vighnesh Raut
-        </a>
-      </p>
+      <p>Made with ❤️ by {getLink('Vighnesh Raut', 'https://vighnesh153.com')}</p>
+      <p>Check out the source code on {getLink('Github', 'https://github.com/vighnesh153/draw.vighnesh153.com')}</p>
     </div>
   );
 }
